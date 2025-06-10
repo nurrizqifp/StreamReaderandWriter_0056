c1682076827ed744142bc096d1afc926e3e4d909
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-10 14:47:47 +0700

    Menggunakan getline untuk mendapatkan input tiap karakter dalam satu baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -17,5 +17,7 @@ int main()
     // unlimited loop untuk menulis
     while(true){
         cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin,baris);
     }
 }
