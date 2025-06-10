60cf5f505e202cac4447838cff97a06848ec93eb
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-06-10 15:19:28 +0700

    Melemparkan (throw) float 0,5

--- manualException.cpp
+++ manualException.cpp
@@ -6,6 +6,7 @@ int main()
     try
     {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
 }
